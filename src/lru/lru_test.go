1ec1fa579a794a454966c66626bb9740bfc9f6a0
zhouzijie <zhouzijie@didiglobal.com>
2021-11-26 16:50:23 +0800

    [add] 添加代码

---
+++ src/lru/lru_test.go
@@ -0,0 +1,56 @@
+package lru
+
+import (
+	"reflect"
+	"testing"
+)
+
+type String string
+
+func (d String) Len() int {
+	return len(d)
+}
+
+func TestGet(t *testing.T) {
+	lru := NewCache(int64(0), nil)
+	lru.Add("key1", String("1234"))
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
+		t.Fatalf("cache hit key1=1234 failed")
+	}
+	if _, ok := lru.Get("key2"); ok {
+		t.Fatalf("cache miss key2 failed")
+	}
+}
+
+func TestRemoveoldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "key3"
+	v1, v2, v3 := "value1", "value2", "value3"
+	cap := len(k1 + k2 + v1 + v2)
+	lru := NewCache(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
+		t.Fatalf("Removeoldest key1 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+
+	lru := NewCache(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("key2", String("k2"))
+	lru.Add("key3", String("k3"))
+	lru.Add("key4", String("k4"))
+
+	except := []string{"key1", "k2"}
+
+	if !reflect.DeepEqual(except, keys) {
+		t.Fatalf("Call Onevicted failed, except keys equals to %s %s", except, keys)
+	}
+}
